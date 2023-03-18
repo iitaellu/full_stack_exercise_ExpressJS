645f47a9b2e55d370f5c2f10904ccf4f407ee302
me mee <me@email.com>
2023-03-18 14:33:55 +0200

    project done

---
+++ Members.js
@@ -0,0 +1,22 @@
+const members = [
+    {
+        id: 1,
+        name: 'Ida Kirveskoski',
+        email: 'ik@email.com',
+        status: 'active'
+    },
+    {
+        id: 2,
+        name: 'Hestia Kissa',
+        email: 'hk@email.com',
+        status: 'inactive'
+    },
+    {
+        id: 3,
+        name: 'Khonsu The Cat',
+        email: 'kTc@email.com',
+        status: 'active'
+    }
+]
+
+module.exports = members;
